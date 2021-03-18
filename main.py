c911ded534044349fc5c9ee8518811a1dd702bb1
Quotoms <azo.cw@yandex.ru>
2021-03-18 18:35:21 +0300

    Подключен интерфейс. Добавлены заглушки.

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
-def open_file():
-    continue
+from PyQt5 import QtWidgets
+from ui.mainframe import Ui_MainWindow
 
 
-def save_file():
-    continue
+class WndProc(QMainWindow):
+    def __init__(self):
+        super(WndProc, self).__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+    def open_file():
+        continue
+
+    def open_file_as():
+        continue
+
+    def save_file():
+        continue
+
+    def save_file_as():
+        continue
+
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication([])
+    application = WndProc()
+    application.show()
+    sys.exit(app.exec())
