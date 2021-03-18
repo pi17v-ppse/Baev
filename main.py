a5ee7b48c3b3c2027eb9e01b308939413ec35c4a
Quotoms <azo.cw@yandex.ru>
2021-03-18 21:59:30 +0300

    Добавление переменной для пути

--- main.py
+++ main.py
@@ -7,12 +7,14 @@ class WndProc(QMainWindow):
         super(WndProc, self).__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-
+        self.path = ''
         self.ui.action_open.connect(self.open_file)
+        self.ui.action_save.connect(self.open_file)
 
     def open_file():
         fileName, _ = QtWidgets.QFileDialog.getOpenFileName(self, "Открыть", "", "Text Files (*.txt)", options = QtWidgets.QFileDialog.Options())
         if fileName:
+            self.path = fileName
             self.ui.plainTextEdit.setText(fileName)
 
     def save_file():
